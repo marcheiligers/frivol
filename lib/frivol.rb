fde08d74d0fff80e997e65cd46a9fb443c21cf97
Marc Heiligers <marc@eternal.co.za>
2010-08-20 13:30:49 +0200

    Moved store_hash and retrieve_hash into Helpers module as they shouldn't be part of the interface

--- lib/frivol.rb
+++ lib/frivol.rb
@@ -3,34 +3,20 @@ require "redis"
 
 module Frivol
   def store(keys_and_values)
-    retrieve_hash
+    Frivol::Helpers.retrieve_hash self
     keys_and_values.each do |key, value|
       @frivol_hash[key.to_s] = value
     end
-    store_hash
+    Frivol::Helpers.store_hash self
   end
   
   def retrieve(keys_and_defaults)
-    retrieve_hash
+    Frivol::Helpers.retrieve_hash self
     result = keys_and_defaults.map do |key, default|
       @frivol_hash[key.to_s] || (respond_to?(default) && send(default)) || default
     end
     return result.first if result.size == 1
     result
-  end
-  
-  def store_hash(hash = nil)
-    @frivol_hash = hash if hash
-    Frivol::Config.redis[storage_key] = @frivol_hash.to_json
-    expire_storage self.class.storage_expiry if @frivol_is_new
-    @frivol_is_new = false
-  end
-  
-  def retrieve_hash
-    return @frivol_hash if defined? @frivol_hash
-    json = Frivol::Config.redis[storage_key]
-    @frivol_is_new = json.nil?
-    @frivol_hash = @frivol_is_new ? {} : JSON.parse(json)
   end
   
   def expire_storage(time)
@@ -54,6 +40,29 @@ module Frivol
     def self.include_in(host_class, storage_expires_in = nil)
       host_class.send(:include, Frivol)
       host_class.storage_expires_in storage_expires_in if storage_expires_in
+    end
+  end
+  
+  module Helpers
+    def self.store_hash(instance)
+      hash = instance.instance_variable_get(:@frivol_hash)
+      is_new = instance.instance_variable_get(:@frivol_is_new)
+      key = instance.send(:storage_key)
+      Frivol::Config.redis[key] = hash.to_json
+      if is_new
+        instance.expire_storage instance.class.storage_expiry
+        instance.instance_variable_set :@frivol_is_new, false
+      end
+    end
+
+    def self.retrieve_hash(instance)
+      return instance.instance_variable_get(:@frivol_hash) if instance.instance_variable_defined? :@frivol_hash
+      key = instance.send(:storage_key)
+      json = Frivol::Config.redis[key]
+      instance.instance_variable_set :@frivol_is_new, json.nil?
+      hash = json.nil? ? {} : JSON.parse(json)
+      instance.instance_variable_set :@frivol_hash, hash
+      hash
     end
   end
   
