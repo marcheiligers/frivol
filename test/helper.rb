6e040ddbf55d6e917cf0f487975f16127706568a
Marc Heiligers <marc@eternal.co.za>
2014-02-19 10:19:50 +0200

    Update with Gemfile and fix testing on Ruby 2.0 with latest gems

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'test/unit'
+require 'mocha/test_unit'
 require 'shoulda'
-require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -11,7 +11,7 @@ class Test::Unit::TestCase
   def fake_redis
     require 'fake_redis'
   end
-  
+
   def ruby_one_eight?
     @ruby_one_eight || `ruby -v`.include?('1.8')
   end
@@ -19,15 +19,15 @@ end
 
 class TestClass
   include Frivol
-  
+
   def id
     1
   end
-  
+
   def save
     store :value => "value"
   end
-  
+
   def load
     retrieve :value => "default"
   end
--- test/test_frivol.rb
+++ test/test_frivol.rb
@@ -1,47 +1,47 @@
 require "#{File.expand_path(File.dirname(__FILE__))}/helper.rb"
 
 class TestFrivol < Test::Unit::TestCase
-  def setup 
+  def setup
     fake_redis # Comment out this line to test against a real live Redis
-    Frivol::Config.redis_config = { :thread_safe => true } # This will connect to a default Redis setup, otherwise set to { :host => "localhost", :port => 6379 }, for example
+    Frivol::Config.redis_config = { :thread_safe => true, :db => 10 } # This will connect to a default Redis setup, otherwise set to { :host => "localhost", :port => 6379 }, for example
     Frivol::Config.redis.flushdb
   end
-  
+
   def teardown
     # puts Frivol::Config.redis.inspect
     Frivol::Config.redis.flushdb
   end
-  
+
   should "have a default storage key made up of the class name and id" do
     t = TestClass.new
     assert_equal "TestClass-1", t.storage_key
   end
-  
+
   should "store and retrieve data" do
     t = TestClass.new
     t.save
     assert_equal "value", t.load
   end
-  
+
   should "return a default for a value that's not in storage" do
     t = TestClass.new
     assert_equal "default", t.load
   end
-  
+
   should "save and retrieve multiple values" do
     class MultiTestClass < TestClass
       def save
         store :val1 => 1, :val2 => 2
       end
-      
+
       def load
         retrieve :val1 => nil, :val2 => nil
       end
     end
-    
+
     t = MultiTestClass.new
     t.save
-    
+
     if ruby_one_eight?
       r = t.load
       assert r == [ 1, 2 ] || r == [ 2, 1 ] # yuck!
@@ -50,30 +50,30 @@ class TestFrivol < Test::Unit::TestCase
       assert_equal [ 1, 2 ], t.load
     end
   end
-  
+
   should "get defaults from instance methods if defined" do
     class DefaultsTestClass < TestClass
       def load
         retrieve :value => nil, :value2 => :value2_default
       end
-      
+
       def value2_default
         "value2"
       end
     end
-    
+
     t = DefaultsTestClass.new
     t.save
     assert_equal [ "value", "value2" ], t.load.sort
   end
-  
+
   should "not be naughty and try to respond to nil default" do
     class DefaultNilTestClass < TestClass
       def load
         retrieve :value => nil
       end
     end
-  
+
     t = DefaultNilTestClass.new
     assert_equal nil, t.load
   end
@@ -83,12 +83,12 @@ class TestFrivol < Test::Unit::TestCase
       def load
         retrieve :value => "load", :def_val => :default_value # yes, that's right, we would cause a stack overflow here
       end
-      
+
       def default_value
         "yay!"
       end
     end
-  
+
     t = DefaultNonSymbolTestClass.new
     assert_equal [ "load", "yay!" ], t.load.sort
   end
@@ -99,34 +99,34 @@ class TestFrivol < Test::Unit::TestCase
         "my_storage"
       end
     end
-    
+
     t = OverrideKeyTestClass.new
     t.save
     assert_equal "value", t.load
     assert Frivol::Config.redis["my_storage"]
   end
-  
+
   should "retain Times as Times" do
     class TimeTestClass < TestClass
       def save
         store :t => Time.local(2010, 8, 20)
       end
-      
+
       def load
         retrieve :t => nil
       end
     end
-  
+
     t = TimeTestClass.new
     t.save
     assert_equal Time.local(2010, 8, 20), t.load
   end
-  
+
   should "expire storage the everytime time it's stored" do
     class ExpiryTestClass < TestClass
       storage_expires_in 60
     end
-    
+
     Frivol::Config.redis.expects(:expire).twice
     Frivol::Config.redis.expects(:ttl).once
     t = ExpiryTestClass.new
@@ -135,7 +135,7 @@ class TestFrivol < Test::Unit::TestCase
     t = ExpiryTestClass.new # get a new one
     t.save
   end
-  
+
   should "expires should not throw nasty errors" do
     t = TestClass.new
     t.save
@@ -144,7 +144,7 @@ class TestFrivol < Test::Unit::TestCase
     t = TestClass.new # Get a fresh instance so that the @frivol_data is empty
     assert_equal "default", t.load
   end
-  
+
   should "use default expiry set on the class" do
     class ExpiryTestClass < TestClass
       storage_expires_in 1
@@ -167,7 +167,7 @@ class TestFrivol < Test::Unit::TestCase
     t.save # a second time
     assert Frivol::Config.redis.ttl(t.storage_key) > 0
   end
-  
+
   should "be able to include in other classes with storage expiry" do
     class BlankTestClass
     end
@@ -176,21 +176,21 @@ class TestFrivol < Test::Unit::TestCase
     assert t.respond_to? :store
     assert_equal 30, BlankTestClass.storage_expiry
   end
-  
+
   should "return the already loaded hash if it's already loaded" do
     Frivol::Config.redis.expects(:[]).once
     t = TestClass.new
     t.load
     t.load
   end
-  
+
   should "be able to delete storage" do
     t = TestClass.new
     t.save
     t.delete_storage
     assert_equal "default", t.load
   end
-  
+
   should "be able to create and use buckets" do
     class SimpleBucketTestClass < TestClass
       storage_bucket :blue
@@ -199,15 +199,15 @@ class TestFrivol < Test::Unit::TestCase
     assert t.respond_to?(:store_blue)
     assert t.respond_to?(:retrieve_blue)
   end
-  
+
   should "store different values in different buckets" do
     class StorageBucketTestClass < TestClass
       storage_bucket :blue
-      
+
       def save_blue
         store_blue :value => "blue value"
       end
-      
+
       def load_blue
         retrieve_blue :value => "blue default"
       end
@@ -233,11 +233,11 @@ class TestFrivol < Test::Unit::TestCase
     class ExpireBucketsTestClass < TestClass
       storage_bucket :blue, :expires_in => 1
       storage_expires_in 3
-      
+
       def save_blue
         store_blue :value => "blue value"
       end
-      
+
       def load_blue
         retrieve_blue :value => "blue default"
       end
@@ -250,7 +250,7 @@ class TestFrivol < Test::Unit::TestCase
     assert_equal "value", t.load
     assert_equal "blue default", t.load_blue
   end
-  
+
   should "be able to create counter buckets" do
     class SimpleCounterTestClass < TestClass
       storage_bucket :blue, :counter => true
@@ -260,15 +260,15 @@ class TestFrivol < Test::Unit::TestCase
     assert t.respond_to?(:retrieve_blue)
     assert t.respond_to?(:increment_blue)
   end
-  
+
   should "store, increment and retrieve integers in a counter" do
     class IncrCounterTestClass < TestClass
       storage_bucket :blue, :counter => true
-      
+
       def save_blue
         store_blue 10
       end
-      
+
       def load_blue
         retrieve_blue 0
       end
@@ -280,11 +280,11 @@ class TestFrivol < Test::Unit::TestCase
     assert_equal 11, t.increment_blue
     assert_equal 11, t.load_blue
   end
-  
+
   should "increment by and retrieve integers in a counter" do
     class IncrCounterByTestClass < TestClass
       storage_bucket :cats, :counter => true
-      
+
       def kittens
         increment_cats_by 5
       end
@@ -299,11 +299,11 @@ class TestFrivol < Test::Unit::TestCase
   should "store, decrement and retrieve integers in a counter" do
     class IncrCounterTestClass < TestClass
       storage_bucket :red, :counter => true
-      
+
       def save_red
         store_red 10
       end
-      
+
       def load_red
         retrieve_red 0
       end
@@ -319,7 +319,7 @@ class TestFrivol < Test::Unit::TestCase
   should "decrement by and retrieve integers in a counter" do
     class DecrCounterByTestClass < TestClass
       storage_bucket :money, :counter => true
-      
+
       def shopping
         decrement_money_by 1000
       end
@@ -336,7 +336,7 @@ class TestFrivol < Test::Unit::TestCase
       storage_bucket :sheep_counter, :counter => true, :expires_in => 1
     end
     t = SetExpireCounterTestClass.new
-    assert_equal 1, SetExpireCounterTestClass.storage_expiry(:sheep_counter)  
+    assert_equal 1, SetExpireCounterTestClass.storage_expiry(:sheep_counter)
   end
 
   should "expire a counter bucket" do
@@ -346,18 +346,18 @@ class TestFrivol < Test::Unit::TestCase
       def bury_kittens
         store_kitten_grave 10
       end
-      
+
       def peek_in_grave
         retrieve_kitten_grave 0
       end
     end
-    
+
     k = ExpireCounterTestClass.new
     k.bury_kittens
     assert_equal 10, k.peek_in_grave
-    
+
     sleep 2
-    assert_equal 0, k.peek_in_grave  
+    assert_equal 0, k.peek_in_grave
   end
 
   should "use seed value for initial value of increment" do
@@ -365,7 +365,7 @@ class TestFrivol < Test::Unit::TestCase
       storage_bucket :cached_count, :counter => true, :seed => Proc.new{ |obj| obj.tedious_count }
 
       def tedious_count
-        99_999 # ... bottle of beers on the wall        
+        99_999 # ... bottle of beers on the wall
       end
     end
 
@@ -378,7 +378,7 @@ class TestFrivol < Test::Unit::TestCase
       storage_bucket :cached_count, :counter => true, :seed => Proc.new{ |obj| obj.tedious_count }
 
       def tedious_count
-        99_999 # ... bottle of beers on the wall        
+        99_999 # ... bottle of beers on the wall
       end
     end
 
@@ -386,13 +386,13 @@ class TestFrivol < Test::Unit::TestCase
     amount = 2
     assert_equal seed.tedious_count + amount, seed.increment_cached_count_by(amount)
   end
-  
+
   should "use seed value for initial value of decrement" do
     class SeedValueCounterTestClass < TestClass
       storage_bucket :cached_count, :counter => true, :seed => Proc.new{ |obj| obj.tedious_count }
 
       def tedious_count
-        99_999 # ... bottle of beers on the wall        
+        99_999 # ... bottle of beers on the wall
       end
     end
 
@@ -405,7 +405,7 @@ class TestFrivol < Test::Unit::TestCase
       storage_bucket :cached_count, :counter => true, :seed => Proc.new{ |obj| obj.tedious_count }
 
       def tedious_count
-        99_999 # ... bottle of beers on the wall        
+        99_999 # ... bottle of beers on the wall
       end
     end
 
@@ -416,46 +416,48 @@ class TestFrivol < Test::Unit::TestCase
 
   should "have thread safe counters" do
     # Note: this test will fail from time to time using fake_redis because fake_redis is not thread safe
-    return if defined? FAKE_REDIS
-    
-    class ThreadCounterTestClass < TestClass
-      storage_bucket :blue, :counter => true
-      
-      def save_blue
-        store_blue 10
-      end
-      
-      def load_blue
-        retrieve_blue 0
-      end
-    end
-    t = ThreadCounterTestClass.new
-    t.save_blue
-    assert_equal 10, t.load_blue
-    
-    threads = []
-    100.times do 
-      threads << Thread.new do
-        10.times do
-          temp = ThreadCounterTestClass.new
-          temp.increment_blue
-          sleep(rand(10) / 100.0)
+    if defined? FAKE_REDIS
+      skip "Skipped because running tests with a fake Redis"
+    else
+      class ThreadCounterTestClass < TestClass
+        storage_bucket :blue, :counter => true
+
+        def save_blue
+          store_blue 10
+        end
+
+        def load_blue
+          retrieve_blue 0
         end
       end
+      t = ThreadCounterTestClass.new
+      t.save_blue
+      assert_equal 10, t.load_blue
+
+      threads = []
+      100.times do
+        threads << Thread.new do
+          10.times do
+            temp = ThreadCounterTestClass.new
+            temp.increment_blue
+            sleep(rand(10) / 100.0)
+          end
+        end
+      end
+      threads.each { |a| a.join }
+
+      assert_equal 1010, t.load_blue
     end
-    threads.each { |a| a.join }
-      
-    assert_equal 1010, t.load_blue
   end
-  
+
   should "be able to delete storage for a bucket" do
     class DeleteBucketTestClass < TestClass
       storage_bucket :silver
-      
+
       def save_silver
         store_silver :value => "value"
       end
-      
+
       def load_silver
         retrieve_silver :value => "default"
       end
@@ -466,15 +468,15 @@ class TestFrivol < Test::Unit::TestCase
     t.delete_silver
     assert_equal "default", t.load_silver
   end
-  
+
   should "be able to clear cached storage for a bucket" do
     class ClearBucketTestClass < TestClass
       storage_bucket :gold
-      
+
       def save_gold
         store_gold :value => "value"
       end
-      
+
       def load_gold
         retrieve_gold :value => "default"
       end
@@ -487,7 +489,7 @@ class TestFrivol < Test::Unit::TestCase
     Frivol::Config.redis.expects(:[]).with(t.storage_key(:gold)).once.returns({ :value => "value" }.to_json)
     assert_equal "value", t.load_gold
   end
-  
+
   should "frivolize methods" do
     class FrivolizeTestClass < TestClass
       def dinosaur_count
@@ -502,13 +504,13 @@ class TestFrivol < Test::Unit::TestCase
     t = FrivolizeTestClass.new
     assert t.methods.include?(ruby_one_eight? ? 'dinosaur_count' : :dinosaur_count)
     assert t.methods.include?(ruby_one_eight? ? 'frivolized_dinosaur_count' : :frivolized_dinosaur_count)
-    
+
     assert_equal 10, t.dinosaur_count
-    
+
     t = FrivolizeTestClass.new # a fresh instance
     assert_equal 10, t.dinosaur_count
   end
-  
+
   should "frivolize methods with expiry in a bucket" do
     class FrivolizeExpiringBucketTestClass < TestClass
       def dinosaur_count
@@ -521,16 +523,16 @@ class TestFrivol < Test::Unit::TestCase
 
     t = FrivolizeExpiringBucketTestClass.new
     assert_equal 10, t.dinosaur_count
-  
+
     t = FrivolizeExpiringBucketTestClass.new # a fresh instance
     assert_equal 10, t.dinosaur_count
-    
+
     sleep 2
-  
+
     t = FrivolizeExpiringBucketTestClass.new # another fresh instance after value expired
     assert_equal 10, t.dinosaur_count
   end
-  
+
   should "frivolize methods with expiry as a counter" do
     class FrivolizeExpiringBucketTestClass < TestClass
       def dinosaur_count
@@ -545,12 +547,12 @@ class TestFrivol < Test::Unit::TestCase
     assert t.methods.include?(ruby_one_eight? ? 'store_dinosaur_count' : :store_dinosaur_count) # check that the bucket name is the method name
 
     assert_equal 10, t.dinosaur_count
-  
+
     t = FrivolizeExpiringBucketTestClass.new # a fresh instance
     assert_equal 10, t.dinosaur_count
-    
+
     sleep 2
-  
+
     t = FrivolizeExpiringBucketTestClass.new # another fresh instance after value expired
     assert_equal 10, t.dinosaur_count
   end
@@ -566,5 +568,5 @@ class TestFrivol < Test::Unit::TestCase
     f = FrivolizeSeedTestClass.new
     assert_equal 88_888 + 1, f.increment_bak_baks
   end
-  
+
 end
